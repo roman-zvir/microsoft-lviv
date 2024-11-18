d7ab77faf75924f29e75c0e632169a69664894e4
Roman Zvir <119957094+Taras-P-Kob@users.noreply.github.com>
2024-11-18 17:22:44 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+document.querySelectorAll('#gallery img').forEach(img => {
+    img.addEventListener('mouseenter', function() {
+        this.style.transform = "scale(1.1)";
+        this.style.transition = "transform 0.3s ease-in-out";
+    });
+
+    img.addEventListener('mouseleave', function() {
+        this.style.transform = "scale(1)";
+    });
+});
 
